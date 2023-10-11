07f17010134ed3b52742f51e0b476acbc5bd1632
Youjiiin <yujinjang24@gmail.com>
2023-10-11 11:15:49 +0900

    JAVA 수업

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.*;
+
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int num = scanner.nextInt();
+//        System.out.println(num);
+//    }
+//}
